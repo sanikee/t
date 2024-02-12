5135ae372d55143dc42247ceb02700db4854b996
sanikee <118053727+sanikee@users.noreply.github.com>
2024-02-12 15:47:56 +0600

    Update utils.js
    добавила сюда перемешивание массивов, из data.js уберу

--- utils.js
+++ utils.js
@@ -1,4 +1,20 @@
 
+// перемешивает массив -------------------------------------------------------------------------
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
 
 // (чёрный/белый) тоггл цвета по яркости -------------------------------------------------------------------------
 
