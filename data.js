6c0a2faa3d3038001133cc6abb15bea00746458e
sanikee <118053727+sanikee@users.noreply.github.com>
2024-02-12 15:50:15 +0600

    Update data.js
    убрала shuffle

--- data.js
+++ data.js
@@ -17,20 +17,3 @@ const backs = [
 
 // перемешивание картинок для разного порядка 
 shuffle(backs);
-
-// перемешивает массив -------------------------------------------------------------------------
-
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-  // While there remain elements to shuffle.
-  while (currentIndex > 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array;
-}
-
